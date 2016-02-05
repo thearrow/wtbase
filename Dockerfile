4974f0d6ea8f829358c22f204827e47abb570eba
Jake Klingler <thearrow3456@gmail.com>
2016-02-05 13:04:58 -0700

    use gunicorn for server

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,7 @@ COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app/
+
+EXPOSE 8000
+
+CMD ["/app/start.sh"]
---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python manage.py migrate
+
+# Prepare log files and start outputting logs to stdout
+touch gunicorn.log
+touch access.log
+tail -n 0 -f *.log &
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn wtbase.wsgi:application \
+    --name web \
+    --bind 0.0.0.0:8000 \
+    --workers 2 \
+    --log-level=info \
+    --log-file=gunicorn.log \
+    --access-logfile=access.log \
+    "$@"
